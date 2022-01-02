fddda1889d218cd7c14ed2f32f83e4610020b24e
Vivek Kumar <95707356+vivek2293@users.noreply.github.com>
2022-01-02 18:41:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function World_Clock(){
+
+document.getElementById('t1').innerHTML = "Your Current Time and Date: ";
+document.getElementById('main1').innerHTML = "Tic-Toc! Tic-Toc! Time is always running ";
+setInterval(() => {
+a = new Date();
+const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+let time = a.getHours() + " : " + a.getMinutes() + " : " + a.getSeconds();
+document.getElementById('time').innerHTML = "Time: " + time;
+
+let date = a.toLocaleDateString(undefined, options);
+document.getElementById('date').innerHTML = "Date: " + date;
+}, 1); 
+
+document.getElementById('timebar').innerHTML = "Thank you for visiting";
+}
